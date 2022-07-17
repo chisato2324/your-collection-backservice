5994b10efa9f8e1a6d2f9a24f93d937761c6aced
chisato2324 <chisato2324>
2022-07-17 23:00:53 +0800

    [add]init

---
+++ src/main/java/cn/chisato/yourcollection/YourCollectionApplication.java
@@ -0,0 +1,13 @@
+package cn.chisato.yourcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourCollectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/chisato/yourcollection/YourCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chisato.yourcollection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
